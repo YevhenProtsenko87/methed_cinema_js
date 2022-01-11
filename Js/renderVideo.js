43dc66a6951c34b4a4ad141a37950f8ae77b3f4f
ProtsenkoYevhen <evgeniu.protsenko@gmail.com>
2022-01-11 14:42:00 +0300

    creating cards

--- Js/renderVideo.js
+++ Js/renderVideo.js
@@ -1,16 +1,19 @@
 import { getTriends } from './services.js';
+import renderCard from './renderCard.js';
+
 
 const filmWeek = document.querySelector('.film-week')
 
 const firstRender = data => {
+    console.log('data: ', data);
 
     filmWeek.innerHTML = `
     <div class="container film-week__container" data-rating="${data.vote_average}">
         <div class="film-week__poster-wrapper">
             <img class="film-week__poster" src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${data.backdrop_path}" alt="постер ${data.name}">
-            <p class="film-week__title_origin">${data.original_title || data.original_name}</p>
+            <p class="film-week__title_origin">${data.original_name || data.original_title}</p>
         </div>
-        <h2 class="film-week__title">${data.title || data.name}</h2>
+        <h2 class="film-week__title">${data.name || data.title}</h2>
         <a class="film-week__watch-trailer tube" href="https://youtu.be/V0hagz_8L3M" aria-label="смотреть трейлер"></a>
     </div>
     `;
@@ -18,8 +21,12 @@ const firstRender = data => {
 
 const renderVideo = async () => {
     const data = await getTriends();
-    
-    firstRender(data.results[0]);
+    console.log('data: ', data);
+    const [ firstCard, ...otherCard ] = data.results;
+    otherCard.length = 12;
+    firstRender(firstCard);
+    renderCard(otherCard)
+    getTriends()
 };
 
 
