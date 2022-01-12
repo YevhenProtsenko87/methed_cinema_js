c5e3ae96b5610a7037db1ec756c289a04879baaf
ProtsenkoYevhen <evgeniu.protsenko@gmail.com>
2022-01-12 13:36:56 +0300

    search

--- Js/services.js
+++ Js/services.js
@@ -2,7 +2,6 @@ const API_KEY = 'a801df85d19f1f04d46de67528171ac9';
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const LANGUAGE = '&language=ru-RU';
 
-//trending/all/day?api_key=<<api_key>>
 
 const getData = url => fetch(url)
   .then(response => {
@@ -13,7 +12,7 @@ const getData = url => fetch(url)
   })
      .catch(err => console.error(err));
 
-export const getTriends = async (type = 'all', period = 'week', page = 1) => {
+export const getTriends = async (type = 'all', period = 'day', page = 1) => {
     const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page${page}`;
     return await getData(url);
 };
@@ -25,5 +24,15 @@ export const getTop = async (type, page = 1) => {
 
 export const getPopular = async (type, page = 1) => {
     const url = `${BASE_URL}${type}/popular?api_key=${API_KEY}${LANGUAGE}&page${page}`;
+    return await getData(url);
+};
+
+export const getVideo = async (id, type) => {
+    const url = `${BASE_URL}${type}/${id}/videos?api_key=${API_KEY}${LANGUAGE}`;
+    return await getData(url);
+};
+
+export const search = async (query, page) => {
+    const url = `${BASE_URL}search/multi?api_key=${API_KEY}${LANGUAGE}&page=${page}&include_adult=false&query=${query}`;
     return await getData(url);
 };
